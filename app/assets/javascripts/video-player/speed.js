00f0ef3c0cae7a2cae22764fd68def81244a3466
iferunsewe <iferunsewe@gmail.com>
2017-01-02 13:17:53 +0000

    Amended speed controls

---
+++ app/assets/javascripts/video-player/speed.js
@@ -0,0 +1,29 @@
+speed = {};
+
+speed.showMenu = function(){
+    var speedButton = $('button#speed');
+    var speedMenu = $('ul#speed-menu');
+    speedButton.click(function(e){
+        speedMenu.toggle();
+    });
+};
+
+speed.setSpeed = function(speed){
+    var speedRateButton = $('ul#speed-menu li button');
+    speedRateButton.click(function(){
+        video.playbackRate = $(this).text();
+    });
+};
+
+$(document).ready(function () {
+    'use strict';
+
+    var supportsVideo = !!document.createElement('video').canPlayType;
+
+    if (supportsVideo) {
+        if (document.addEventListener) {
+            speed.showMenu();
+            speed.setSpeed();
+        }
+    }
+});
